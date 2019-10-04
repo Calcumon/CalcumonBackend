c7a9d7c5cb51304ad4818ca7162ab8b7b3a6a727
Michael McGowan <michael.mcgowan@students.makeschool.com>
2019-10-04 13:20:26 -0700

    new users created, fix public_id

--- manage.py
+++ manage.py
@@ -1,12 +1,16 @@
 import os
 import unittest
 
+from app import blueprint
+
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
 from app.main import create_app, db
+from app.main.model import user
 
 app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
+app.register_blueprint(blueprint)
 
 app.app_context().push()
 
